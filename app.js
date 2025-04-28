e4cc6d786dfe56a1f2ea14dec516ef1997a9b51e
Tomas Lacomba <166060058+tomaslacomba@users.noreply.github.com>
2025-04-28 15:57:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+/* var  no se usa
+
+ forma correcta
+
+ let <- para variables de cualquier tipo
+
+const <- para constantes de cualquier tipo //
+
+ var nombre = 1;
+ nombre = "cadena de texto";
+
+ console.log (typeof nombre);
+
+ let a = true;
+ let b = true;
+
+ console.log("not a: " , !a);
+console.log("not b: ", !b);
+
+ console.log ("OR a o b:", a || b);
+
+ let a = true;
+ let b = true;
+
+ console.log("not a: " , !a);
+console.log("not b: ", !b);
+
+console.log ("OR a o b:", a || b);
+
+console.log ("and a y b:", a && b);
+
+
+console.log("suma: ", a + b);
+console.log ("resta: ", a - b);
+console.log ("multiplicacion: ", a * b);
+console.log ("division: ", a / b);
+console.log ("modulo: ", a % b); 
+
+
+
+//concatenacion
+
+let a = "1";
+let b = 1;
+
+console.log(a + b)
+
+let numbers = [1, 2, 3, "hola", true];
+
+console.log(numbers);*/
+
+const numbers =[1, 2, 3, 4 ];
+let numers2 = numbers;
+
+numers2 = [1, 2, 3, 4];
+
+console.log(numbers);
+console.log(numers2);
+
+console.log();
+
